f916aaa1d1bce83c2dc144e0b756f1c123bf53ae
Derek Johnson <howpopsir@gmail.com>
2024-10-08 20:46:53 -0400

    Added name and environment changes.

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+       <h1>Derek Johnson</h1> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
