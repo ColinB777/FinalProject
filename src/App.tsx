f827936a7ed5fd1df6796f8627190d1b1fea5b1b
Colin <rabcol@udel.edu>
2024-10-14 14:56:50 -0400

    Fixing App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,40 +23,33 @@ function App() {
     window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
   }
 
-  function testButtonPrint()
-  {
-    alert("Sending back to home page... (TEST)");
-  }
-
   //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
   return (
-    <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Colin Barry </h1>
-      <h1> Derek Johnson </h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          <h1>Matias Sayanes</h1>
-          <h1> Samuel Zheng </h1>
-        </p>
-        <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-        Learn React
-
-        </a>
+    <div className="App" style={{background:'lime'}}>
+    <Router>
+      <header className="App">
+        <h4> Colin Barry,Matias Sayanes,Samuel Zheng,Derek Johnson </h4>
+      </header>
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
+      <Button style={{background:'white'}}>
+      <Link to="/detailed_quiz" style={{color:'magenta'}}>Detailed quiz</Link>
+      </Button>
+      <Button  style={{background:'white'}}>
+      <Link to="/basic_quiz">Quiz</Link>
+      </Button>
+      <Routes>
+        <Route path="/detailed_quiz" element={<Detailedquiz />} />
+        <Route path="/basic_quiz" element={<SimpleInterface/>} />
+      </Routes>
+    </Router>
     </div>
   );
 }
