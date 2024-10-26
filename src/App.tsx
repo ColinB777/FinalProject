42aff3b2869c10d458a5b0197a917f2a186cd5f3
matis <matiass@udel.edu>
2024-10-26 19:07:16 -0400

    Implemented the API processing of the answers to get a report.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ let keyData = "";
 const saveKeyData = "MYKEY";
 const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
 if (prevKey !== null) {
-  keyData = JSON.parse(prevKey);
+  keyData = prevKey;
 }
 
 export function App() {
