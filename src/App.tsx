f356527243809566daec816b5adf3c2b301d14d2
Colin <rabcol@udel.edu>
2024-11-08 14:39:02 -0500

    Merging

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
 import './progressBar.css'
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form} from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import { Detailedquiz } from "./Detailed";
+import {Detailedquiz} from "./Detailed"
+// import { HomeTest } from './Home';
 import Results from "./SimpleQuestions/Results";
-import Question1 from "./SimpleQuestions/1";
-import Question2 from './SimpleQuestions/2';
+import Question1  from "./SimpleQuestions/1"
+import  Question2  from './SimpleQuestions/2';
 import Question3 from './SimpleQuestions/3';
 import Question4 from './SimpleQuestions/4';
 import Question5 from './SimpleQuestions/5';
 import Question6 from './SimpleQuestions/6';
-import Question7 from './SimpleQuestions/7';
+import Question7  from './SimpleQuestions/7';
 
 import { BsFillHouseFill } from "react-icons/bs";
 import { BsFileBarGraph } from "react-icons/bs";
@@ -22,32 +23,38 @@ import { BsFileBarGraph } from "react-icons/bs";
 let keyData = "";
 const saveKeyData = "MYKEY";
 const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
+
 if (prevKey !== null) {
   keyData = JSON.parse(prevKey);
 }
 console.log(keyData);
 
+
 export function App() {
-  const [key, setKey] = useState<string>(keyData); // For API key input
+  const [key, setKey] = useState<string>(keyData); //for api key input
+  // const [barState, setBar] = useState<number>(0);
   const [buttonState, setVisible] = useState<boolean>(true);
   const [responses, setResponses] = useState<{ [key: string]: string }>({});
   
-  // Sets the local storage item to the API key the user inputed
+  //sets the local storage item to the api key the user inputed
   function handleSubmit() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); // Reloading to refresh local storage data
+    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
   }
-
-  // Updates local state of key when user changes it
+  
+  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
 
-  function hideButtons() {
+  function hideButtons()
+  {
     setVisible(!buttonState);
   }
 
+
   return (
+    
     <body>
     <div>
 
@@ -70,18 +77,25 @@ export function App() {
       <div className = 'menu-bar'>
         
 
-              <Button style={{ background: 'white', margin: 10 }} id="HOME" onClick={hideButtons}>
-                <Link to="/">Home</Link>
-              </Button>
+      <Button className = "Career-Btn" hidden={buttonState} onClick={hideButtons}>
+      <Link to="/detailed_quiz">Detailed Career Assesment</Link>
+      </Button>
 
-              <Button className="Career-Btn" id="HIDE_BASIC" hidden={buttonState} onClick={hideButtons}>
-                <Link to="/basic_quiz">Basic Career Assessment</Link>
-              </Button>
-            </div>
-            <hr />
-          </div>
+      <Button className = "home-btn" id="HOME" onClick={hideButtons}>
+        <Link to="/"><BsFillHouseFill /></Link>
+      </Button>
 
-          <Routes>
+
+      <Button className = "Career-Btn" id="HIDE_BASIC" hidden={buttonState} onClick={hideButtons}>
+      <Link to="/basic_quiz">Basic Career Assesment</Link>
+      </Button>
+
+      </div>
+
+
+      </div>
+
+      <Routes>
           <Route path="/detailed_quiz" element={<Detailedquiz />} />
             <Route path="/basic_quiz" element={<Question1 responses={responses} setResponses={setResponses} />} />
             <Route path="/SimpleQuestions/Question2" element={<Question2 responses={responses} setResponses={setResponses} />} />
@@ -94,17 +108,23 @@ export function App() {
           </Routes>
         </Router>
 
-        <div className="page-bottom">
-          <Form>
-            <Form.Label style={{ fontSize: 10 }}>API Key:</Form.Label>
-            <Form.Control style={{ width: 300, fontSize: 10 }} type="password" placeholder="Insert API Key Here" onChange={changeKey} />
-            <br />
-            <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-          </Form>
-        </div>
+    <div className = "page-bottom">
+    {/*<footer>Colin Barry,Matias Sayanes,Samuel Zheng,Derek Johnson</footer>*/}
+
+    <Form>
+        <Form.Label style = {{fontSize:10}}>API Key:</Form.Label>
+        <Form.Control style = {{width:300, fontSize:10}}type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+        <br></br>
+        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+      </Form>
       </div>
+
+
+
+    </div>
     </body>
   );
 }
+
 
 export default App;
