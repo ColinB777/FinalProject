7c0129558230d0c1bc2cb7496b6ef24cee618959
matias <matiass@udel.edu>
2024-11-20 16:55:28 -0500

    created a worse result screen but with better codebase

--- src/DetailedQuestions/detailed-result.tsx
+++ src/DetailedQuestions/detailed-result.tsx
@@ -11,43 +11,59 @@ if (StoredReport){
 }
 
 
+
 export function DetailedResult( ):React.JSX.Element{
     console.log("This is the report:  " , Report)
     const [displayedSeg,setDisplayedSeg]=useState<number>(0);
+    const SectionedReport = Report.slice(1);
+
+    const SummarySec ={
+      header: SectionedReport[0].slice(0,SectionedReport[0].indexOf("\n")),
+
+      subsections: SectionedReport[0]
+      .slice(SectionedReport[0].indexOf("\n")+2)
+      .split("**")
+      .filter((item,index) => (index !== 0) && item !== null)
+      
+    };
+
+    const CareerPathsSec = {
+      header: SectionedReport[1].slice(0,SectionedReport[1].indexOf("\n")),
+      subsections: SectionedReport[1].slice(SectionedReport[1].indexOf("\n")+1)
+      .split(/(?=\d+\.)/)
+      .filter((item,index) => (index !== 0) && item !== null )
+      .map(item => item.replaceAll("**","").replaceAll("-",""))
+    };
+
+    const Conclusion = {
+      header: SectionedReport[2].slice(0,SectionedReport[2].indexOf("\n")),
+
+      subsections: SectionedReport[2]
+      .slice(SectionedReport[2].indexOf("\n")+2)
+      .split("**")
+      .filter(item => item !== null)
+    };
+    
     return <div>
+
         <Button onClick={()=>setDisplayedSeg(0)}>Assessment Summary</Button>
         <Button onClick={()=>setDisplayedSeg(1)}>Recommended Career Paths</Button>
         <Button onClick={()=>setDisplayedSeg(2)}>Conclusion</Button>
-        {Report.slice(1).map((segment:string,i:number) =>(
-        (displayedSeg===i)?
-        <div>
-          {/* Section Header */}
-          <h1>{segment.slice(0,segment.indexOf("\n"))}</h1>
-          {/* Section Body */}
-          {(segment.slice(segment.indexOf("\n")+1)
-            .split("**")
-              .map((subheader:string,segI:number)=>(
-              //this ternary if will return true if the section is Recommended Career paths
-              (i !== 1) ?
-              //this ternary if will return true if the text is a subheader 
-                (segI%2 === 1) ?
-                <h3 style={{whiteSpace: "break-spaces" , margin:25  }}>{subheader}</h3>
-                :
-                <span  style={{whiteSpace: "break-spaces"}}>{subheader.trim()} </span>
-              :
-                //this ternary if will return true if the text is a subheader 
-                (segI%2 === 1) ?
-                  //this ternary if will return true if the text is a Career path
-                  (subheader !== "Actionable Steps:" && subheader !== "Reason:" ) ?
-                  <h3 style={{whiteSpace: "break-spaces" , marginTop:25,marginBottom:-20  }}>{subheader}</h3>
-                  :
-                  <h4 style={{whiteSpace: "break-spaces" , margin:25  }}>{subheader}</h4>
-                :
-                <span  style={{whiteSpace: "break-spaces"}}>{subheader.replaceAll("-", "").trim()} </span>
-            )))}
-        </div>
-        : null
-      ))}
+        
+      {(displayedSeg === 0) ? <div>
+          <h1>{SummarySec.header}</h1>
+          <body style={{whiteSpace: "break-spaces"}}>{SummarySec.subsections}</body>
+        </div> : null}
+
+        {(displayedSeg === 1) ? <div>
+          <h1>{CareerPathsSec.header}</h1>
+          <body style={{whiteSpace: "break-spaces"}}>{CareerPathsSec.subsections}</body>
+        </div> : null}
+        
+        {(displayedSeg === 2) ? <div>
+          <h1>{Conclusion.header}</h1>
+          <body style={{whiteSpace: "break-spaces"}}>{Conclusion.subsections}</body>
+        </div> : null}
       </div>
 
 }
